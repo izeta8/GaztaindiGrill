069533705bd328c0f988f488fd1fcf14fc6e7960
izeta8 <jiz.j12.a4@gmail.com>
2026-09-08 20:02:44 +0200

    feat: add a close button to toast notifications and fix its position

--- GaztaindiGrill-NextJS/src/app/providers.tsx
+++ GaztaindiGrill-NextJS/src/app/providers.tsx
@@ -15,7 +15,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
         <RunningProgramsProvider>
           <CurrentModeProvider>
           {children}
-          <Toaster position="top-center" richColors />
+          <Toaster position="top-center" richColors closeButton />
           </CurrentModeProvider>
         </RunningProgramsProvider>
       </GrillStateProvider>
